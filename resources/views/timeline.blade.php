5883f3804b8e8e0b846061e897c62eac9a35b238
Mahib22 <mahib.arib@gmail.com>
2022-01-31 12:02:41 +0700

    modifikasi tampilan sedikit

---
+++ resources/views/timeline.blade.php
@@ -0,0 +1,16 @@
+<x-app-layout>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden border sm:rounded-lg">
+                <div class="p-6 bg-white border-gray-200">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem praesentium culpa deleniti?
+                    Accusantium placeat quibusdam suscipit quam cumque possimus atque ex, reprehenderit, aliquam
+                    reiciendis porro. Consectetur numquam ipsa eligendi enim. Lorem ipsum, dolor sit amet consectetur
+                    adipisicing elit. Dolores, dicta assumenda illum similique tempora at culpa? Exercitationem
+                    perspiciatis, repellat est explicabo maiores accusantium, laboriosam impedit nostrum numquam nobis
+                    voluptates facilis!
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
